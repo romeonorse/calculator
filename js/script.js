35ce6880c1f09715538a7a4ce422ec2bc19c2708
romeonorse <romeonorse@gmail.com>
2022-05-24 21:03:13 +0300

    Add basic math functions

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const calcButtons = document.querySelectorAll('.calc-button');
+const displayText = document.querySelector('.display-text');
+
+// BASIC MATH
+const add = (a, b) => a + b;
+const substr = (a, b) => a - b;
+const mult = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let result;
+
+// BUTTON AND DISPLAY EVENTS
+calcButtons.forEach(button => {
+  button.addEventListener('click', function(e) {
+    if (displayText.innerText.length < 10 && e.target.innerText !== '=') {
+      displayText.innerText += e.target.innerText;
+    } else {
+      displayText.innerText = 'SUPERERROR';
+    }
+
+    let displayString = displayText.innerText;
+    let pressedSymbols;
+
+    if (displayString.includes('/')) {
+      pressedSymbols = displayString.split('/');
+      result = divide(pressedSymbols[0],pressedSymbols[1]);
+    } else if (displayString.includes('*')) {
+      pressedSymbols = displayString.split('*');
+      result = mult(pressedSymbols[0],pressedSymbols[1]);
+    } else if (displayString.includes('-')) {
+      pressedSymbols = displayString.split('-');
+      result = substr(pressedSymbols[0],pressedSymbols[1]);
+    } else if (displayString.includes('+')) {
+      pressedSymbols = displayString.split('+');
+      result = add(+pressedSymbols[0],+pressedSymbols[1]);
+    }
+
+    return result;
+  })
+})
+
+// RESULT =
+const resultButton = document.querySelector('#result');
+resultButton.addEventListener('click', function() {
+  displayText.innerText = result.toFixed(3);
+})
+
+// CLEAR PREVIOUS
+const clearbutton = document.querySelector('#reset');
+clearbutton.addEventListener('click', function() {
+  displayText.innerText = '';
+})
